99a703658885dde1e415536a857b7038f5b6db47
Bruce-jimenez <jimenezbruce2022@gmail.com>
2023-01-17 19:06:34 -0500

    Express established

---
+++ utilities/data.js
@@ -0,0 +1,7 @@
+const todoData = [
+    {description: "Cleaning", complete: true},
+    {description: "Laundry", complete: true},
+    {description: "Organizing room", complete: true}
+]
+
+module.exports = todoData;
